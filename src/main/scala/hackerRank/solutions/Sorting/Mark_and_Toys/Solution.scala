0c87604e9771a1ed989ee184e772aaf93af6f7bd
eungwan <eungwan@tmon.co.kr>
2020-04-09 01:07:08 +0900

    no message

--- src/main/scala/hackerRank/solutions/Sorting/Mark_and_Toys/Solution.scala
+++ src/main/scala/hackerRank/solutions/Sorting/Mark_and_Toys/Solution.scala
@@ -15,55 +15,55 @@ import java.util.stream._
   */
 object Solution {
 
-    // Complete the maximumToys function below.
-    def maximumToys(prices: Array[Int], k: Int): Int = {
-        import scala.util.control.Breaks._
-        var count = 0
-        var totalPrice = 0
-        println(prices.sorted.toList)
-        breakable {
-            for (price <- prices.sorted) {
-                totalPrice += price
-                if (totalPrice >= k) {
-                    break
-                }
-                count += 1
-                println(totalPrice, count)
-            }
+  // Complete the maximumToys function below.
+  def maximumToys(prices: Array[Int], k: Int): Int = {
+    import scala.util.control.Breaks._
+    var count = 0
+    var totalPrice = 0
+    println(prices.sorted.toList)
+    breakable {
+      for (price <- prices.sorted) {
+        totalPrice += price
+        if (totalPrice >= k) {
+          break
         }
-        count
+        count += 1
+        println(totalPrice, count)
+      }
     }
+    count
+  }
 
-    def main(args: Array[String]): Unit = {
-        val arguments = Array(
-            ("1 12 5 111 200 1000 10".split(" ").map(_.toInt), 50) // 4
-        )
-        arguments.foreach {
-            case (a1, a2) =>
-                val rst = maximumToys(a1, a2)
-                println(rst)
-        }
+  def main(args: Array[String]): Unit = {
+    val arguments = Array(
+      ("1 12 5 111 200 1000 10".split(" ").map(_.toInt), 50) // 4
+    )
+    arguments.foreach {
+      case (a1, a2) =>
+        val rst = maximumToys(a1, a2)
+        println(rst)
     }
+  }
 
-    /*
-    def main(args: Array[String]) {
-        val stdin = scala.io.StdIn
+  /*
+  def main(args: Array[String]) {
+      val stdin = scala.io.StdIn
 
-        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+      val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
 
-        val nk = stdin.readLine.split(" ")
+      val nk = stdin.readLine.split(" ")
 
-        val n = nk(0).trim.toInt
+      val n = nk(0).trim.toInt
 
-        val k = nk(1).trim.toInt
+      val k = nk(1).trim.toInt
 
-        val prices = stdin.readLine.split(" ").map(_.trim.toInt)
-        val result = maximumToys(prices, k)
+      val prices = stdin.readLine.split(" ").map(_.trim.toInt)
+      val result = maximumToys(prices, k)
 
-        printWriter.println(result)
+      printWriter.println(result)
 
-        printWriter.close()
-    }
-    */
+      printWriter.close()
+  }
+  */
 }
 
