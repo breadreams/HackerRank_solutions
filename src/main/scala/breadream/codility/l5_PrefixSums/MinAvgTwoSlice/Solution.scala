00b8cb3aa1269059f750b104e41bd981fd35eb5b
eungwan <eungwan@tmon.co.kr>
2020-04-09 19:05:40 +0900

    no message

--- src/main/scala/breadream/codility/l5_PrefixSums/MinAvgTwoSlice/Solution.scala
+++ src/main/scala/breadream/codility/l5_PrefixSums/MinAvgTwoSlice/Solution.scala
@@ -9,29 +9,69 @@ package breadream.codility.l5_PrefixSums.MinAvgTwoSlice
   * A pair of integers (P, Q), such that 0 ≤ P < Q < N, is called a slice of array A (notice that the slice contains at least two elements). The average of a slice (P, Q) is the sum of A[P] + A[P + 1] + ... + A[Q] divided by the length of the slice.
   * To be precise, the average equals (A[P] + A[P + 1] + ... + A[Q]) / (Q − P + 1).
   *
+  * For example, array A such that:
   *
+  * A[0] = 4
+  * A[1] = 2
+  * A[2] = 2
+  * A[3] = 5
+  * A[4] = 1
+  * A[5] = 5
+  * A[6] = 8
+  * contains the following example slices:
   *
+  * slice (1, 2), whose average is (2 + 2) / 2 = 2;
+  * slice (3, 4), whose average is (5 + 1) / 2 = 3;
+  * slice (1, 4), whose average is (2 + 2 + 5 + 1) / 4 = 2.5.
+  * The goal is to find the starting position of a slice whose average is minimal.
   *
+  * Write a function:
   *
+  * object Solution { def solution(a: Array[Int]): Int }
   *
+  * that, given a non-empty array A consisting of N integers, returns the starting position of the slice with the minimal average. If there is more than one slice with a minimal average, you should return the smallest starting position of such a slice.
+  *
+  * For example, given array A such that:
+  *
+  * A[0] = 4
+  * A[1] = 2
+  * A[2] = 2
+  * A[3] = 5
+  * A[4] = 1
+  * A[5] = 5
+  * A[6] = 8
+  * the function should return 1, as explained above.
+  *
+  * Write an efficient algorithm for the following assumptions:
+  *
+  * N is an integer within the range [2..100,000];
+  * each element of array A is an integer within the range [−10,000..10,000].
   */
 object Solution {
   def solution(A: Array[Int]): Int = {
+
+    A.sliding(2, 1)
+    A.sliding(3, 1)
+    A.sliding(4, 1)
+
+
+    println(s"A=[${A.toList.mkString(",")}]")
     // A value range [−10,000..10,000]
     // N range [2..100,000]
     val N = A.length
 
-
-
-
-
-
-
+    val SUM = Array.fill[Int](N + 1){0}
+    for (i <- 1 to N) {
+      SUM(i) = SUM(i - 1) + A(i - 1)
+    }
+    println(s"SUM=[${SUM.toList.mkString(",")}]")
 
     0
   }
 
-  
+  def main(args: Array[String]): Unit = {
+    println(solution(Array(4,2,2,5,1,5,8)))
+  }
 }
 
 
