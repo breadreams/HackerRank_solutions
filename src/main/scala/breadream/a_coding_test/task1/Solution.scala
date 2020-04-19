e9e2b2092cbed580ec4711ab9c87f28f5e9a562e
eungwan <eungwan@tmon.co.kr>
2020-04-19 13:08:42 +0900

    no message

---
+++ src/main/scala/breadream/a_coding_test/task1/Solution.scala
@@ -0,0 +1,27 @@
+package breadream.a_coding_test.task1
+
+/**
+  * Solution Description
+  *
+  *
+  *
+  *
+  *
+  */
+object Solution extends App {
+  
+  def solution(a: Array[Int]): Int = {
+    0
+  }
+
+  def solution2(a: Array[Int]): Int = {
+    0
+  }
+
+  // test case
+  println(solution(Array(1,2,3,4,5)))
+  println(solution(Array(1,2,3,4,5,6)))
+
+  println(solution2(Array(1,2,3,4,5)))
+  println(solution2(Array(1,2,3,4,5,6)))
+}
--- src/main/scala/breadream/codility/l6_Sorting/Triangle/Solution.scala
+++ src/main/scala/breadream/codility/l6_Sorting/Triangle/Solution.scala
@@ -14,12 +14,28 @@ object Solution extends App {
     // each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
     // A empty check
     if (a.isEmpty || a.length < 3) return 0
+    //println(a.sorted.toList)
     a.sorted.sliding(3, 1).foreach {
       case Array(p, q, r) =>
         if (p.toLong + q.toLong > r.toLong)
           return 1
     }
     0
+  }
+
+  def count(a: Array[Int]): Int = {
+    // N is an integer within the range [0..100,000];
+    // each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+    // A empty check
+    if (a.isEmpty || a.length < 3) return 0
+    //println(a.sorted.toList)
+    var count = 0
+    a.sorted.sliding(3, 1).foreach {
+      case Array(p, q, r) =>
+        if (p.toLong + q.toLong > r.toLong)
+          count += 1
+    }
+    count
   }
 
   def solution2(b: Array[Int]): Int = {
@@ -40,11 +56,14 @@ object Solution extends App {
 
   def solution3(b: Array[Int]): Int = {
     val a = b.sorted
+    println(a.toList)
+    println(count(a))
     val n = a.length
     var result = 0
     for (x <- a.indices) {
       var z = x + 2
       for (y <- x + 1 until n) {
+        println(x, y, z)
         import scala.util.control.Breaks._
         breakable {
           while (z < n) {
@@ -53,9 +72,10 @@ object Solution extends App {
             z += 1
           }
         }
-        println(s"x[$x],y[$y],z[${z-1}] => hit(${z - y - 1})")
+        //println(s"x[$x],y[$y],z[${z-1}] => hit(${z - y - 1})")
         //println(z - y - 1)
-        result += z - y - 1
+        if (z - y - 1 > 0)
+          result += z - y - 1
       }
     }
     result
@@ -64,7 +84,7 @@ object Solution extends App {
 //  println(solution(Array(10,2,5,1,8,20)))
 //  println(solution(Array(1,1,1,1,8,20)))
 //  println(solution(Array(1,2,3)))
-//  println(solution(Array(-3,-2,-4,3,-3, 2,3,4)))
+  println(solution(Array(-3,-2,-4,3,-3, 2,3,4)))
 
   println
 
